9dc6bc24e93524ba91c89cc0cb12e9e4b339d76f
caleben <frankia5n@163.com>
2020-03-07 11:10:34 +0800

    create delete table

---
+++ src/main/java/Constant.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author wenci 2020/3/7 7:19
+ */
+public class Constant {
+    public static final Integer AMOUNT = 100;
+    public static final String NAMESPACE = "x";
+    public static final String FAMILY = "f";
+    public static final String TABLE = "COURSE_INFO";
+    public static final List<String> COURSE = Arrays.asList("English","Chinese", "Physical", "Chemistry", "Math", "History","Political");
+    public static final List<String> TEACHER = Arrays.asList("Tom","Kong zi","Einstein","Harry potter","Fourier","Mr SiMa","Napoleon");
+    public static final List<String> COLUMNS = Arrays.asList("course", "teacher");
+}
--- src/main/java/HBaseInput.java
+++ src/main/java/HBaseInput.java
@@ -1,6 +1,10 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.slf4j.Logger;
@@ -16,7 +20,11 @@ public class HBaseInput {
     private static final Logger LOG = LoggerFactory.getLogger(HBaseInput.class);
     public static final String CONF_DIR = "conf";
     public static final String USER_DIR = System.getProperty("user.dir");
-    private Configuration conf;
+    private Connection connection;
+
+    public HBaseInput() {
+        this.connection = createConnection(createHadoopConf());
+    }
 
     private Configuration createHadoopConf() {
         String path = USER_DIR + File.separator + CONF_DIR;
@@ -24,8 +32,6 @@ public class HBaseInput {
         Configuration conf;
         conf = HBaseConfiguration.create();
         conf.addResource(new Path(path, "hbase-site.xml"));
-        conf.addResource(new Path(path, "core-site.xml"));
-        conf.addResource(new Path(path, "hdfs-site.xml"));
         conf.set("hbase.client.scanner.timeout.period", "90000");
         return conf;
     }
@@ -36,5 +42,47 @@ public class HBaseInput {
         } catch (IOException e) {
             throw new RuntimeException("get HBase connection error!!!", e);
         }
+    }
+
+    private void input() throws IOException {
+        Admin admin = connection.getAdmin();
+        HColumnDescriptor columnDescriptor = new HColumnDescriptor(Constant.FAMILY);
+        HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(Constant.NAMESPACE, Constant.TABLE));
+
+    }
+
+    private void createTable(String namespace, String table, byte[][] splits) {
+        try {
+            Admin admin = connection.getAdmin();
+            TableName tableName = TableName.valueOf(namespace, table);
+            if (!admin.tableExists(tableName)) {
+                HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
+                tableDescriptor.addFamily(new HColumnDescriptor(Constant.FAMILY));
+                admin.createTable(tableDescriptor, splits);
+                LOG.info("===== create table completed [{}] =====", tableName.toString());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("get hbase admin error!!!", e);
+        }
+    }
+
+    private void deleteTable(String namespace, String table) {
+        try {
+            Admin admin = connection.getAdmin();
+            TableName tableName = TableName.valueOf(namespace, table);
+            if (admin.tableExists(tableName)) {
+                admin.disableTable(tableName);
+                admin.deleteTable(tableName);
+            }
+            LOG.info("===== delete table completed [{}] =====", tableName.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("get HBase admin error!!!", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        HBaseInput baseInput = new HBaseInput();
+        byte[][] splits = {"01".getBytes()};
+        baseInput.createTable(Constant.NAMESPACE, Constant.TABLE, splits);
     }
 }
